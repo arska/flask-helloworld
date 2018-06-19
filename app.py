144224aafe71eb662bd32fa7e14cd73b07d3b730
Aarno Aukia <aarno.aukia@vshn.ch>
2018-06-19 16:12:45 +0200

    make the message configurable

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return os.environ.get("MESSAGE", "Hello World!")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=8080)
