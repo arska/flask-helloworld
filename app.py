cc36f300f72232d87b114af679a6c47686324b9b
Aarno Aukia <aarno.aukia@vshn.ch>
2018-06-26 12:03:04 +0300

    restore hello world default message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return os.environ.get("MESSAGE", "Hello IBM conference!")
+    return os.environ.get("MESSAGE", "Hello world!")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=8080)
