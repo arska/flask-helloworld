e8a37134d66b10437b09a7eb7c5ee283be7de6e7
Aarno Aukia <aarno.aukia@vshn.ch>
2018-07-09 11:48:57 +0300

    add tox (pylint) testing, clean up code accordingly

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
+"""
+Tiny hello world service example
+"""
+
 import os
 from flask import Flask
-app = Flask(__name__)
 
-@app.route("/")
+APP = Flask(__name__)  # Standard Flask app
+
+
+@APP.route("/")
 def hello():
+    """
+    Hello world on root path
+    """
     return os.environ.get("MESSAGE", "Hello world!")
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=8080)
+    APP.run(host='0.0.0.0', port=os.environ.get('listenport', 8080))
